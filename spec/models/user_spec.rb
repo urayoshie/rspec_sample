557c1f45ece7aef2b84566c91fadd1a11e768145
Yoshie <yoshieura@icloud.com>
2021-01-09 22:15:21 +0900

    RSpecテストコードの実装

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "名前の表示" do
+    # 共通するパラメーターの定義
+    let(:params) {{name: "satou"}}
+    it "年齢1 : ユーザーの名前が取得できること" do
+      # merge!メソッドで後からパラメータを追加
+      params.merge!(age: 1)
+      user = User.new(params)
+      expect(user.disp_name).to eq "satouちゃん"
+    end
+    it "年齢15 : ユーザーの名前が取得できること" do
+      # merge!メソッドで後からパラメータを追加
+      params.merge!(age: 15)
+      user = User.new(params)
+      expect(user.disp_name).to eq "satou君"
+    end
+    it "年齢20 : ユーザーの名前が取得できること" do
+      # merge!メソッドで後からパラメータを追加
+      params.merge!(age: 20)
+      user = User.new(params)
+      expect(user.disp_name).to eq "satouさん"
+    end
+  end
+end
